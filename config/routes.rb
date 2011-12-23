509c1156442f814ad3d4d2f635e3f012cdb39a61
Joe Foley <foley@ru.is>
2011-12-23 20:02:39 +0100

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  #get "users/new"  ## old users access
+  resources :users  #new users route
 
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/home', :to => 'pages#home'
+
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
