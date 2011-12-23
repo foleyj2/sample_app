529400eeafd9a8f4a11367032a40cc54e017c7aa
Joe Foley <foley@ru.is>
2011-12-23 02:32:08 +0100

    checkpoint before sleep

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+
+  # commented out to ensure a red test
+  #validates :name, :presence => true
+end
