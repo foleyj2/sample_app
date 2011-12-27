459888f91ea2be6eeb5266e70298650f5d60ed5f
Joe Foley <foley@ru.is>
2011-12-27 15:24:47 +0100

    moving exercise into appropriate branche static-pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -39,19 +39,4 @@ describe PagesController do
     end
   end
 
-  # exercise 3.5.1
-  describe "GET 'help'" do
-    it "should be successful" do
-      get 'help'
-      response.should be_success
-    end
-    
-    it "should have the right title" do
-      get 'help'
-      response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | Help")
-    end
-    
-  end #help
-
 end
