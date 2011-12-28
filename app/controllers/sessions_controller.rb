0aab5bd7e7908a83e49cf609916f129d2ce936cb
Joe Foley <foley@ru.is>
2011-12-28 16:20:16 +0100

    friendly forwarding from 10.2.4 now working and tested

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,9 @@ class SessionsController < ApplicationController
       render 'new'
     else # valid user
       sign_in user
-      redirect_to user
+      #redirect_to user
+      #replaced by friendly forwarding (Listing 10.18)
+      redirect_back_or user
     end
   end #create
 
